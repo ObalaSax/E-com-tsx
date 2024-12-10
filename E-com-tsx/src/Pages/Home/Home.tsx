0309a14f17b1694db8845ce90b816d2d66e53bd0
Obala Sax <obala.music@gmail.com>
2024-12-10 16:47:34 +0300

    Ghai

--- E-com-tsx/src/Pages/Home/Home.tsx
+++ E-com-tsx/src/Pages/Home/Home.tsx
@@ -1,37 +1,41 @@
 import { useEffect, useState } from "react";
-import ProductCard from "../../Components/ProductCard/ProductCard";
 
 interface ProductDataProps {
   id: number;
   title: string;
-  image: string;
-  price: number;
 }
 
 function Home() {
   const [products, setProducts] = useState<ProductDataProps[]>([]);
+
   useEffect(() => {
     const fetchProductData = async () => {
       try {
         const response = await fetch("https://dummyjson.com/products");
-        const myProductsdata: ProductDataProps[] = await response.json();
-        const products = myProductsdata;
-        setProducts(myProductsdata);
-
-        console.log("Hello", products);
-        console.log("Tuko sawa", myProductsdata);
+        const fetchedProducts = (await response.json()) as ProductDataProps[];
+        setProducts(fetchedProducts);
       } catch (error) {
-        console.error("Failed to access Products", error);
+        console.error("Bro Noma", error);
       }
     };
     fetchProductData();
-  });
+  }, []);
+
+  console.log(products);
 
   return (
     <div className="home">
       <div className="home-container">
         <h1>Home</h1>
-        <ProductCard />
+        <ul>
+          {products.length > 0 ? (
+            products.map((product) => (
+              <li key={product.id}> {product.title}</li>
+            ))
+          ) : (
+            <p>Loading products...</p>
+          )}
+        </ul>
       </div>
     </div>
   );
